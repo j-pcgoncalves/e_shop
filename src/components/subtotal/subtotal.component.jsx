1f770793b7fddf6d0834abe4e4fdcfa6eb7416f8
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-18 22:56:10 +0100

    create checkout page

--- src/components/subtotal/subtotal.component.jsx
+++ src/components/subtotal/subtotal.component.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import './subtotal.styles.css';
+import CurrencyFormat from 'react-currency-format';
 
 const Subtotal = () => {
     return (
-        <div>
-
+        <div className="subtotal">
+            <CurrencyFormat
+                renderText= {(value) => (
+                    <>
+                        <p>
+                            Subtotal (0 items): <strong>$0</strong>
+                        </p>
+                        <small className="subtotal__gift">
+                            <input type='checkbox' /> This order contains a gift!
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={0}
+                displayType={'text'}
+                thousandSeparator={true}
+            />
         </div>
     )
 }
