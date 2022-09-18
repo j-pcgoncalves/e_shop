baf7c549f1d62ef5321e044f851294a0d257b627
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-19 00:01:48 +0100

    update subtotal dinamically

--- src/components/subtotal/subtotal.component.jsx
+++ src/components/subtotal/subtotal.component.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import './subtotal.styles.css';
 import CurrencyFormat from 'react-currency-format';
+import { useStateValue } from '../stateProvider/stateProvider.component';
+import { getBasketTotal } from '../reducer/reducer';
 
 const Subtotal = () => {
+    
+    const [{basket}, dispatch] = useStateValue();
+    
     return (
         <div className="subtotal">
             <CurrencyFormat
                 renderText= {(value) => (
                     <>
                         <p>
-                            Subtotal (0 items): <strong>$0</strong>
+                            Subtotal ({basket.length} items): <strong>${value}</strong>
                         </p>
                         <small className="subtotal__gift">
                             <input type='checkbox' /> This order contains a gift!
@@ -17,7 +22,7 @@ const Subtotal = () => {
                     </>
                 )}
                 decimalScale={2}
-                value={0}
+                value={getBasketTotal(basket)}
                 displayType={'text'}
                 thousandSeparator={true}
             />
