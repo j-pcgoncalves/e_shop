0a835eb5de3bba78a31be3a61f1a5ee39285376b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-18 19:14:01 +0100

    create header component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import './App.css';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div className="App">
-      <div className='header_logo'>
-        
-      </div>
-
-      <div className='header_search'>
-
-      </div>
-
-      <div className='header_nav'>
-        <div className='nav_item'>
-          <span className='nav_itemLineOne'>Hello Guest</span>
-          <span className='nav_itemLineTwo'>Sign In</span>
-        </div>
-        <div className='nav_item'>
-          <span className='nav_itemLineOne'>Your</span>
-          <span className='nav_itemLineTwo'>Shop</span>
-        </div>
-        <div className='nav_item'>
-          <span className='nav_itemLineOne'>Basket</span>
-          <span className='nav_itemLineTwo'>0</span>
-        </div>
-      </div>
-
+      <Header />
     </div>
   );
 }
