f7255fdb5487492fa16c32bdc2bdba8f60056e28
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-18 21:00:45 +0100

    passing props to product component

--- src/components/product/product.component.jsx
+++ src/components/product/product.component.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import './product.styles.css';
 
-const Product = () => {
+const Product = ({ id, title, image, price, rating }) => {
     return (
         <div>
             <div className="product">
-                <div className="product_info">
-                    <p>Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)</p>
-                    <p className="product_price">
+                <div className="product__info">
+                    <p>{title}</p>
+                    <p className="product__price">
                         <small>$</small>
-                        <strong>30</strong>
+                        <strong>{price}</strong>
                     </p>
-                    <div className="product_rating">⭐⭐</div>
+                    <div className="product__rating">
+                        {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐</p>
+                        ))}
+                    </div>
                 </div>
 
-                <img src='https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg' alt='Product' />
+                <img src={image} alt='Product' />
 
                 <button>Add to Basket</button>
             </div>
