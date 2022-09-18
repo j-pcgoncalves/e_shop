572faaef649e34f4fa18f597d036f85a859c9b1f
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-18 21:27:53 +0100

    chenging home styles

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -52,7 +52,7 @@ const Home = () => {
 
                     <div className='home__row'>
                         <Product
-                            id="90829332"
+                            id="6"
                             title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
                             price={1094.98}
                             rating={4}
