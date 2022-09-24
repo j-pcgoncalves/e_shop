e124846c11929b58bf328312571bad030d4fa412
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-24 22:30:48 +0100

    initialize firebase App

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyB__Mc74oGqc8p-Nb3Hej-PKvENuMVyjY0",
     authDomain: "eshop-fe3c1.firebaseapp.com",
@@ -6,3 +8,9 @@ const firebaseConfig = {
     messagingSenderId: "159454390793",
     appId: "1:159454390793:web:e27783d1804f1a8a8be6d8"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
