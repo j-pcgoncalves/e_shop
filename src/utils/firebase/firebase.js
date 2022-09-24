23facc64bd26bb07d2122b1428db07b3753f79d5
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-24 22:28:11 +0100

    create firebase.js file

---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB__Mc74oGqc8p-Nb3Hej-PKvENuMVyjY0",
+    authDomain: "eshop-fe3c1.firebaseapp.com",
+    projectId: "eshop-fe3c1",
+    storageBucket: "eshop-fe3c1.appspot.com",
+    messagingSenderId: "159454390793",
+    appId: "1:159454390793:web:e27783d1804f1a8a8be6d8"
+};
